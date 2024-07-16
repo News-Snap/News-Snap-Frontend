fa9a06ba893ea38aadd765d3bc4ae2e599552647
JYLeem <jinyoungim105@gmail.com>
2024-07-16 16:40:06 +0900

    Feat: Initialise Project

---
+++ News-Snap/News-Snap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-Snap
+//
+//  Created by Jinyoung Leem on 7/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
