50a02e00b3d3e4bc6517de8762d2f0ce506fabaf
JYLeem <jinyoungim105@gmail.com>
2024-07-18 21:33:08 +0900

    Feat: Scrap 모델 생성

--- News-Snap/News-Snap/Model/Scrap.swift
+++ News-Snap/News-Snap/Model/Scrap.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Scrap {
+    let id : Int
+    var link : String
+    var contents : String
+    var keywords : [String]
+    
+    var attachmentFile : FileManager?
+    var referenceFile : FileManager?
+    
+    init(id: Int, link: String, contents: String, keywords: [String]) {
+        self.id = id
+        self.link = link
+        self.contents = contents
+        self.keywords = keywords
+    }
+    
+    
+    
+}
+
