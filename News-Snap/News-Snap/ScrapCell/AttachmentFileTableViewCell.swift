33300517f177616132b5b958f46657b83003462a
JYLeem <jinyoungim105@gmail.com>
2024-08-08 21:54:19 +0900

    Feat: 테이블 셀 생성

---
+++ News-Snap/News-Snap/ScrapCell/AttachmentFileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AttachmentFileTableViewCell.swift
+//  News-Snap
+//
+//  Created by Jinyoung Leem on 8/8/24.
+//
+
+import UIKit
+
+class AttachmentFileTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var AttachmentFilePath: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
