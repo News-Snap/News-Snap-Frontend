5544eee8cbc1b2530917b1257dbbdb415bb6339a
kayeon2 <tjsrkdus@gmail.com>
2024-08-08 21:52:59 +0900

    Feat: 키워드 보드 테이블 뷰 구성

---
+++ News-Snap/News-Snap/home/KeywordBoardViewController.swift
@@ -0,0 +1,33 @@
+//
+//  KeywordBoardViewController.swift
+//  News-Snap
+//
+//  Created by 선가연 on 8/8/24.
+//
+
+import UIKit
+
+class KeywordBoardViewController: UIViewController {
+
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var keywordLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
