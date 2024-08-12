34add31c75377610b96ad7ee83600114bbd2ccda
jhhyuen <daniel101115@naver.com>
2024-08-12 21:23:14 +0900

    Error: TableViewController와 Cell 연결

---
+++ News-Snap/News-Snap/Controller/Scrap_DB/MyScrapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyScrapViewController.swift
+//  News-Snap
+//
+//  Created by 문정현 on 8/12/24.
+//
+
+import UIKit
+
+class MyScrapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+  
+
+}
---
+++ News-Snap/News-Snap/Model/Scrap_DB_Model.swift
@@ -0,0 +1,8 @@
+//
+//  Scrap_DB_Model.swift
+//  News-Snap
+//
+//  Created by 문정현 on 8/12/24.
+//
+
+import Foundation
