b16e4b7496bc817f6abaf89551e5d7b9ffee5e0a
kayeon2 <tjsrkdus@gmail.com>
2024-08-17 15:18:44 +0900

    Feat: 서브 홈 화면 구성

---
+++ News-Snap/News-Snap/Contents/ContentsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContentsViewController.swift
+//  News-Snap
+//
+//  Created by 선가연 on 8/17/24.
+//
+
+import UIKit
+
+class ContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
