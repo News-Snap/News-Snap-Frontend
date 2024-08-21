23fc3b3b91ab2e39e77b7ce5736132e162e332d0
kayeon2 <tjsrkdus@gmail.com>
2024-08-21 17:02:48 +0900

    Feat: API 연결

---
+++ News-Snap/News-Snap/home/Model/popularStocks.swift
@@ -0,0 +1,23 @@
+//
+//  popularStocks.swift
+//  News-Snap
+//
+//  Created by 선가연 on 8/17/24.
+//
+
+struct Stock: Decodable {
+    let stock: String
+    let rank: Int
+}
+
+struct PopularStocksResponse: Decodable {
+    let isSuccess: Bool
+    let code: String
+    let message: String
+    let result: ResultData
+    
+    struct ResultData: Decodable {
+        let popularStocks: [Stock]
+    }
+}
+
